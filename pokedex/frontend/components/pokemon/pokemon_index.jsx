c047d01662a0b2e0fd4f76c9dd6c4b0552288cf1
App Academy Student <student@appacademy.io>
2017-09-14 13:26:43 -0700

    render pokemon in list

---
+++ pokedex/frontend/components/pokemon/pokemon_index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import PokemonIndexItem from "./pokemon_index_item";
+
+class PokemonIndex extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.requestAllPokemon();
+  }
+
+  render() {
+    console.log(this.props);
+    const {pokemon} = this.props;
+    const pokemonItems = this.props.pokemon.map(
+      poke => <PokemonIndexItem key={poke.id} pokemon={poke} /> );
+
+    return (
+      <div>
+        <section className="pokedex">
+        <ul>
+          {pokemonItems}
+        </ul>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default PokemonIndex;
